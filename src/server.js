252df6a648e7fa22ff245d442a14db6aa620f7ac
namhazover9 <88364629+namhazover9@users.noreply.github.com>
2024-11-14 15:43:42 +0700

    feat/B4B-14: Product CRUD

--- src/server.js
+++ src/server.js
@@ -5,11 +5,18 @@ dotenv.config();
 const cors = require("cors");
 const port = process.env.PORT || 8000;
 const configViewEngine = require("./config/configViewEngine");
+
+//route
+const productRoutes = require("./routes/productRoute");
+
 const app = express();
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//route
+app.use("/products", productRoutes);
 //View Engine
 configViewEngine(app);
 
