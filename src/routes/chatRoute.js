8a71abc86994e5c725b160937d3dfd1ee81233d0
vu hoang nam <hoagnam150303@gmail.com>
2024-12-10 11:16:47 +0700

    fix chat API

--- src/routes/chatRoute.js
+++ src/routes/chatRoute.js
@@ -7,4 +7,5 @@ router.post("/createChat/:id", isAuth, chatController.createChat);
 router.put("/updateChat/:id/:userId", isAuth, chatController.updateChat);
 router.delete("/deleteChat/:id/:userId", isAuth, chatController.deleteChat);
 router.get("/getAllChatById/:id", isAuth, chatController.getAllChatById);
+router.get("/getChatById/:id", isAuth, chatController.getChatById);
 module.exports = router;
